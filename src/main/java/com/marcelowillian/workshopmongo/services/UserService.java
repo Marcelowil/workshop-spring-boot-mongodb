3d50707b5bd62c5fdba1cb0ca21ed205f99bb69a
Marcelo William <marcelo.willian@live.com>
2024-02-12 12:03:44 -0300

    Inserção de usuário com POST

--- src/main/java/com/marcelowillian/workshopmongo/services/UserService.java
+++ src/main/java/com/marcelowillian/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcelowillian.workshopmongo.domain.User;
+import com.marcelowillian.workshopmongo.dto.UserDTO;
 import com.marcelowillian.workshopmongo.repository.UserRepository;
 import com.marcelowillian.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,12 @@ public class UserService {
 		return user;
 	}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
