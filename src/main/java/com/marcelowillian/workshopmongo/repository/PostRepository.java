30cf519cef81790177936bdcec90f29854e67a05
Marcelo William <marcelo.willian@live.com>
2024-02-14 07:57:49 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/marcelowillian/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.marcelowillian.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcelowillian.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
